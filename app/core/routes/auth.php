bd4d33845df3a8ed1dcca106a9a3cf0cb87ed718
mafof <pavel.bykov2018@mail.ru>
2018-12-23 16:28:58 +0300

    add logout

--- app/core/routes/auth.php
+++ app/core/routes/auth.php
@@ -2,7 +2,7 @@
 
 use Pecee\SimpleRouter\SimpleRouter as Router;
 
-Router::get('/login', function() {
+Router::match(['get', 'post'], '/login', function() {
    include_once "app/controllers/controller_login.php";
    (new App\Controllers\ControllerLogin())->action();
 });
@@ -10,4 +10,10 @@ Router::get('/login', function() {
 Router::match(['get', 'post'], '/register', function() {
     include_once "app/controllers/controller_register.php";
     (new App\Controllers\ControllerRegister())->action();
+});
+
+Router::get('/logout', function () {
+   session_destroy();
+   setcookie('PHPSESSID', null);
+   header('Location: /');
 });
