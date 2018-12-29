6267e43cf9e01405aadc987cb5ab69e9aebc8772
mafof <pavel.bykov2018@mail.ru>
2018-12-30 00:50:05 +0300

    fix bug with don't show posts to unauthorized users

--- app/models/model_main.php
+++ app/models/model_main.php
@@ -6,16 +6,17 @@ use App\Core\DB\PostDB;
 
 class ModelMain extends BaseModel {
     public function get_data() {
-        if(parent::isAuth()) {
-            $postDB = new PostDB(true);
-            $listPosts = $postDB->getPosts(10, 0);
-            $postDB->closeDB();
+        $dataUser = array();
+        if(parent::isAuth()) $dataUser = parent::getDataUser();
 
-            if(!empty($listPosts)) {
-                return array_merge(parent::getDataUser(), ['posts' => $listPosts]);
-            } else {
-                return parent::getDataUser();
-            }
+        $postDB = new PostDB(true);
+        $listPosts = $postDB->getPosts(10, 0);
+        $postDB->closeDB();
+
+        if(!empty($listPosts)) {
+            return array_merge($dataUser, ['posts' => $listPosts]);
+        } else {
+            return $dataUser;
         }
     }
 }
