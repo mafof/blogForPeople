ba6d6b27959b7fbbd3334037561d203fad46598f
mafof <pavel.bykov2018@mail.ru>
2019-01-21 21:55:51 +0300

    fix bug in naming class name ModelConfirmAccount

--- App/Controllers/ControllerConfirmAccount.php
+++ App/Controllers/ControllerConfirmAccount.php
@@ -2,11 +2,11 @@
 namespace App\Controllers;
 
 use App\Core\BaseController;
-use App\Models\ModelConfrimAccount;
+use App\Models\ModelConfirmAccount;
 
 class ControllerConfirmAccount extends BaseController {
     public function action() {
-        $this->model = new ModelConfrimAccount();
+        $this->model = new ModelConfirmAccount();
         $data = $this->model->get_data();
         $this->view->generate("view_confirm_account.php", $data, null);
     }
