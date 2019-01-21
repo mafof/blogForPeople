3e64cc5872684ed744bb5feac9d07182958dcd14
mafof <pavel.bykov2018@mail.ru>
2019-01-21 22:30:54 +0300

    fix bug with show post to category named post english

--- App/Models/ModelShowPostToCategory.php
+++ App/Models/ModelShowPostToCategory.php
@@ -9,7 +9,8 @@ class ModelShowPostToCategory extends BaseModel {
     public function get_data() {
         $postDb = new PostDB(true);
         $data = $postDb->getPostsToCategory(TranslateConverterCyrillic::translateToRussian($GLOBALS['categoryName']));
-
+        if(empty($data)) $data = $postDb->getPostsToCategory($GLOBALS['categoryName']);
+        
         $userData = parent::getDataUser();
         $userData = ($userData == null) ? [] : $userData;
 
